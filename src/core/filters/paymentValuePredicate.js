31ea09cbdb09acee49ffa663d071bcce3e8ce185
Irina Turenko <turenko7irina@mail.ru>
2021-10-16 13:57:12 +0300

    fixed bug

--- src/core/filters/paymentValuePredicate.js
+++ src/core/filters/paymentValuePredicate.js
@@ -1,24 +1,22 @@
 export const PaymentValuePredicate = (props, offers, value) => {
-  const minValidLoanValue = props.filters.loanValue * offers.minInitialPayment || undefined
-  const maxValidLoanValue = props.filters.loanValue * offers.maxInitialPayment || undefined
-  if (minValidLoanValue === undefined|| props.filters.loanValue === undefined) {
+  const minValidLoanValue =
+    props.filters.loanValue * offers.minInitialPayment || undefined
+  const maxValidLoanValue =
+    props.filters.loanValue * offers.maxInitialPayment || undefined
+  if (
+    minValidLoanValue === undefined ||
+    props.filters.loanValue === undefined
+  ) {
     return true
   }
-    if (value >= minValidLoanValue && value <= maxValidLoanValue) {
-      return true
-    }
-    if (value < minValidLoanValue) {
-      return false
-    } if (value > maxValidLoanValue) {
-      return false
-    }
+  if (value >= minValidLoanValue && value <= maxValidLoanValue) {
+    return true
   }
+  if (value < minValidLoanValue) {
+    return false
+  }
+  if (value > maxValidLoanValue) {
+    return false
+  }
+}
     
-
-
-
-// примеры тестов на предикат кредит
-// true инпут пустой
-// true инпут между максимальным и минимальным значениями оффера
-// false инпут меньше минимаольного
-// false инпут больше максимального
--- src/core/filters/predicates.js
+++ src/core/filters/predicates.js
@@ -3,6 +3,7 @@ import { realtyTypePredicate } from './realtyTypePredicate'
 import { productPredicate } from './productPredicate'
 import { insurancePredicate } from './insurancePredicate'
 import { loanValuePredicate } from './loanValuePredicate'
+import { PaymentValuePredicate } from './PaymentValuePredicate'
 
 export const predicates = {
   banks: banksPredicate,
@@ -10,4 +11,5 @@ export const predicates = {
   product: productPredicate,
   insurance: insurancePredicate,
   loanValue: loanValuePredicate,
+  paymentValue: PaymentValuePredicate,
 }
